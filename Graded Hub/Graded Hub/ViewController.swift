80828b8be27841b860493a41de142f2a31015ebe
Emmett Shaughnessy <emmett0502@gmail.com>
2018-10-21 21:47:56 -0500

    Initial Creation and UI Start

---
+++ Graded Hub/Graded Hub/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Graded Hub
+//
+//  Created by Emmett Shaughnessy on 10/21/18.
+//  Copyright © 2018 Emmett Shaughnessy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
